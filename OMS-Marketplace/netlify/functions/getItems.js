747947ae8923fdf4eda24b482db4de0dd2b256b6
c3p0r2d2luke <doesthislongemailannoyu@gmail.com>
2025-12-03 23:07:31 +0000

    deleted

--- OMS-Marketplace/netlify/functions/getItems.js
+++
@@ -1,5 +0,0 @@
-const { read } = require('./_helpers');
-exports.handler = async () => {
-const items = read('items.json');
-return {statusCode:200,body:JSON.stringify(items)};
-};
--- OMS-Marketplace/netlify/functions/getPending.js
+++
@@ -1,5 +0,0 @@
-const { read } = require('./_helpers');
-exports.handler = async () => {
-const pending = read('pending.json');
-return {statusCode:200,body:JSON.stringify(pending)};
-};
